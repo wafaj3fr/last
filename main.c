4c4a60c67c5fcbdb00d0ce21b7e280316e986fe8
wafa j3fr <wafaagaafar@gmail.com>
2023-11-03 15:46:03 +0200

    new initial commit

--- main.c
+++ main.c
@@ -9,30 +9,7 @@
 
 int main(int ac, char **argv)
 {
-    /*char *prompt_of_us = "The_shell_is_this $";*/
-    char *line = NULL, *line_cp = NULL;
-    /*const char *delimmeter = " \n";
-    int num_pars = 0;
-    char *pars;*/
-    size_t n = 0;
-    ssize_t chread;
-    int i;
-    __pid_t pid;
-
-    (void)ac;
-
-    while (1)
-    {
-
-        prompt_of_us();
-        chread = getline(&line, &n, stdin);
-
-        if (chread == -1)
-        {
-            write(1, "\n", 1);
-            break;
-        }
-    }
+    printf("shell");
 
     return (0);
 }
--- usd_prompt.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-
-void prompt_of_us(void)
-{
-    char *prompt = "wash$ ";
-    write(1, prompt, _strlen(prompt));
-}
