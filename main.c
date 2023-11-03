afd6715e425528a9945819765abbc4fb8cf307c5
wafa j3fr <wafaagaafar@gmail.com>
2023-11-03 23:35:45 +0200

    commit

--- main.c
+++ main.c
@@ -9,19 +9,19 @@
 
 int main(int ac, char **argv)
 {
-	char *line = NULL;
-	size_t n = 0;
-	ssize_t stread;
-	int i;
+    char *line = NULL;
+    size_t n = 0;
+    ssize_t stread;
 
-	(void)ac;
+    (void)ac;
+    (void)argv;
 
-	while (1)
-	{
-		if(isatty(STDIN_FILENO))
-			write(1, "$ ", 2);
-		stread = getline(&line, &n, stdin);
-	}
-
-
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            write(1, "$ ", 2);
+        stread = getline(&line, &n, stdin);
+        printf("%s", line);
+    }
+    return (0);
 }
