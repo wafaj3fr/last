fb01ec4328e3a6f29af14e6ce68eeab42513f3d8
wafa j3fr <wafaagaafar@gmail.com>
2023-11-03 15:47:29 +0200

    main.h updated

--- main.h
+++ main.h
@@ -6,10 +6,8 @@
 #include <stdlib.h>
 
 int _strlen(char *s);
-void parse(char *line, ssize_t chread);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _strcmp(const char *s1, const char *s2);
-void prompt_of_us(void);
 
 #endif
