177db241a68ef21e13610f4278ce44a5335c21c0
Batman99SD <azozkrg1@gmail.com>
2023-11-03 22:31:23 +0300

    sa

--- main.c
+++ main.c
@@ -9,7 +9,19 @@
 
 int main(int ac, char **argv)
 {
-    printf("shell");
+	char *line = NULL;
+	size_t n = 0;
+	ssize_t stread;
+	int i;
 
-    return (0);
+	(void)ac;
+
+	while (1)
+	{
+		if(isatty(STDIN_FILENO))
+			write(1, "$ ", 2);
+		stread = getline(&line, &n, stdin);
+	}
+
+
 }
