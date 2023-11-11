f9c16f5ce6b7404ae7bfee8542bf5e1416b19a0e
wafa j3fr <wafaagaafar@gmail.com>
2023-11-11 22:13:31 +0200

    edits

--- execute.c
+++ execute.c
@@ -4,9 +4,6 @@ void exec_wa(char *path, char *const *argv)
 {
     pid_t pid;
     int status;
-    char **new_argv;
-    size_t full_path_len = strlen(path) + 1;
-    char *full_path = malloc(full_path_len);
 
     pid = fork();
     if (pid == -1)
@@ -15,11 +12,6 @@ void exec_wa(char *path, char *const *argv)
     }
     else if (pid == 0)
     {
-        strcpy(full_path, path);
-
-        new_argv = malloc((sizeof(char *) * 2));
-        new_argv[0] = full_path;
-        new_argv[1] = NULL;
 
         if (execve(path, argv, NULL) == -1)
         {
